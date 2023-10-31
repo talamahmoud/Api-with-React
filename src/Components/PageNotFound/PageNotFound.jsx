7a58e6b5e851381209b53e2164c2e7c5f307ece2
talamahmoud <tala.ismail.kafa@gmail.com>
2023-10-31 23:08:45 +0200

    PageNotFound fix

--- src/Components/PageNotFound/PageNotFound.jsx
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PageNotFound() {
   return (
     <>
-        <img src='./PageNotFound.webp' className='pt-5  m-auto d-block' />
+        <img src='./PageNotFound.png' className='pt-5  m-auto d-block' />
     </>
   )
 }
